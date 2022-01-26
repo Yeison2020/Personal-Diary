1f45c4225a80687e7efa928455c8bed7f1dec3ec
Yeison <enmanuelquiterios@gmail.com>
2022-01-26 15:58:17 -0500

    Added Changes

--- client/src/Components/miniCardsEntries/MiniCards.js
+++ client/src/Components/miniCardsEntries/MiniCards.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./miniCards.css";
-
+// git push heroku main
 const miniCards = ({ data }) => {
+  const handleDelete = (id) => {};
+  console.log(data);
   return (
     <div>
       <div className="container-mini-cards animate__animated animate__swing">
         <p className="name-cards">{data.title}</p>
         <p className="date-cards">{data.date}</p>
+        <button className="delete-btn">Delete</button>
       </div>
     </div>
   );
